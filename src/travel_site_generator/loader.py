424b834da3a9e38a2d41e9f4689df9c3eb953cd4
Thomas Leese <thomas@leese.io>
2026-04-21 13:38:46 +0100

    Support loading trips recursively

--- src/travel_site_generator/loader.py
+++ src/travel_site_generator/loader.py
@@ -13,8 +13,9 @@ def load_journal(path: Path) -> Journal:
 
     trips = []
 
-    for trip_path in (path / "trips").glob("*.md"):
-        logger.info("Loading trip from %s", trip_path.name)
+    for trip_path in (path / "trips").glob("*/*.md"):
+        name = str(trip_path)[len(str(path)) + 7 :]
+        logger.info("Loading trip from %s", name)
         trips.append(trip_parser.load(trip_path))
 
     return Journal(trips=trips)
