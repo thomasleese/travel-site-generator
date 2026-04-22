18620f547c9bfdbe46a26cbc2e5a0408872426ac
Thomas Leese <thomas@leese.io>
2026-04-22 21:10:26 +0100

    Add a basic timeline
    Closes #2

--- src/travel_site_generator/generator.py
+++ src/travel_site_generator/generator.py
@@ -1,10 +1,14 @@
+import datetime
 import json
 import logging
+import shutil
 from pathlib import Path
+from typing import Any
 
 import jinja2
 from markupsafe import Markup
-import mistune
+from mistune.util import escape as escape_text
+from mistune import HTMLRenderer as BaseHTTPRenderer, Markdown
 
 from .routes import Routes
 from .timeline import Timeline
@@ -12,6 +16,13 @@ from .trips import Trips
 
 
 logger = logging.getLogger(__name__)
+
+
+def write_static(dst_path: Path):
+    logger.info("Copying static files to %s", dst_path)
+    dst_path.mkdir(parents=True, exist_ok=True)
+    src_path = Path(__file__).parent.resolve() / "static"
+    shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
 
 
 def write_geojson(trips: Trips, routes: Routes, path: Path):
@@ -40,26 +51,57 @@ def write_geojson(trips: Trips, routes: Routes, path: Path):
         file.write(json.dumps(data))
 
 
-def generate(trips: Trips, routes: Routes, timeline: Timeline, path: Path):
-    logger.info("Saving to %s", path)
+class HTMLRenderer(BaseHTTPRenderer):
+    def heading(self, text: str, level: int, **attrs: Any) -> str:
+        tag = "h" + str(level + 1)
+        html = "<" + tag
+        _id = attrs.get("id")
+        if _id:
+            html += ' id="' + escape_text(_id) + '"'
+        return html + ">" + text + "</" + tag + ">\n"
 
-    path.mkdir(parents=True, exist_ok=True)
 
-    write_geojson(trips, routes, path / "data.json")
+def write_index_html(trips: Trips, routes: Routes, timeline: Timeline, path: Path):
+    template_loader = jinja2.PackageLoader("travel_site_generator")
 
     env = jinja2.Environment(
-        loader=jinja2.PackageLoader("travel_site_generator", "templates"),
+        loader=template_loader,
         autoescape=jinja2.select_autoescape(),
     )
 
-    def markdown(value):
-        return Markup(mistune.markdown(value))
+    markdown = Markdown(renderer=HTMLRenderer(escape=False))
 
-    env.filters["markdown"] = markdown
+    def to_date(origin: datetime.date, destination: datetime.date) -> str:
+        if origin == destination:
+            return origin.strftime("%-d %B %Y")
+
+        destination_str = destination.strftime("%-d %B %Y")
+
+        if origin.month == destination.month and origin.year == destination.year:
+            origin_str = origin.strftime("%-d")
+        elif origin.year == destination.year:
+            origin_str = origin.strftime("%-d %B")
+        else:
+            origin_str = origin.strftime("%-d %B %Y")
+
+        return f"{origin_str} – {destination_str}"
+
+    env.filters["to_date"] = to_date
+    env.filters["markdown"] = lambda value: Markup(markdown(value))
 
     template = env.get_template("index.html")
 
     index_html = template.render(trips=trips, routes=routes, timeline=timeline)
 
-    with open(path / "index.html", "w") as file:
+    with open(path, "w") as file:
         file.write(index_html)
+
+
+def generate(trips: Trips, routes: Routes, timeline: Timeline, path: Path):
+    logger.info("Saving to %s", path)
+
+    path.mkdir(parents=True, exist_ok=True)
+
+    write_static(path / "static")
+    write_index_html(trips, routes, timeline, path / "index.html")
+    write_geojson(trips, routes, path / "data.json")
--- src/travel_site_generator/timeline.py
+++ src/travel_site_generator/timeline.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 import logging
 from typing import Optional
 
-from .journeys import Journey, Stop, ModeOfTransport
+from .journeys import Journey, Stop, ModeOfTransport, JourneyLeg
 from .places import Place
 from .trips import Trip, Trips
 
@@ -12,36 +12,38 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
-class TimelineStop:
-    place: Place
-    date: datetime.date
-    mode_of_transport: Optional[ModeOfTransport]
+class TimelineEntry:
+    place: Optional[Place] = None
+    origin_date: Optional[datetime.date] = None
+    destination_date: Optional[datetime.date] = None
+    mode_of_transport: Optional[ModeOfTransport] = None
 
     @staticmethod
-    def from_stop(
-        stop: Stop, mode_of_transport: Optional[ModeOfTransport]
-    ) -> TimelineStop:
-        return TimelineStop(
-            place=stop.place, date=stop.date, mode_of_transport=mode_of_transport
+    def from_stop(stop: Stop) -> TimelineEntry:
+        return TimelineEntry(place=stop.place)
+
+    @staticmethod
+    def from_leg(leg: JourneyLeg) -> TimelineEntry:
+        return TimelineEntry(
+            origin_date=leg.origin.date,
+            destination_date=leg.destination.date,
+            mode_of_transport=leg.mode_of_transport,
         )
 
 
 @dataclass(frozen=True)
 class TimelineJourney:
-    stops: list[TimelineStop]
+    entries: list[TimelineEntry]
 
     @staticmethod
     def from_journey(journey: Journey) -> TimelineJourney:
-        stops = [TimelineStop.from_stop(journey.origin, mode_of_transport=None)]
+        entries = [TimelineEntry.from_stop(journey.origin)]
 
         for leg in journey.legs:
-            stops.append(
-                TimelineStop.from_stop(
-                    leg.destination, mode_of_transport=leg.mode_of_transport
-                )
-            )
+            entries.append(TimelineEntry.from_leg(leg))
+            entries.append(TimelineEntry.from_stop(leg.destination))
 
-        return TimelineJourney(stops=stops)
+        return TimelineJourney(entries=entries)
 
 
 @dataclass(frozen=True)
